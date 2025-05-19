d14504336dbd805f6ada122d9b195a51a6bcd110
song <sooong137@gmail.com>
2025-05-19 17:12:04 +0900

    ..

--- src/main/java/member/member/MemberApplication.java
+++ src/main/java/member/member/MemberApplication.java
@@ -4,9 +4,11 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableDiscoveryClient
 @EnableJpaAuditing // Auditing 활성화
 @SpringBootApplication
 public class MemberApplication {
