0e209f9f216114fd54448b17e7058b47b9b0ae84
song <sooong137@gmail.com>
2025-05-23 15:37:01 +0900

    ..

--- src/main/java/member/member/MemberApplication.java
+++ src/main/java/member/member/MemberApplication.java
@@ -5,9 +5,11 @@ import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients(basePackages = "member.member.client")
 @EnableDiscoveryClient
 @EnableJpaAuditing // Auditing 활성화
 @SpringBootApplication
